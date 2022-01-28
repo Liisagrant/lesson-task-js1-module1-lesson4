8bbd1f43b6eb46dd13e66e06a9c3d4044ef01a97
Liisagrant <lisagrant1990@gmail.com>
2022-01-28 10:52:51 +0100

    started on lesson task 1,2,3,4,5

--- js/script.js
+++ js/script.js
@@ -13,3 +13,83 @@ const games = [
         released: 2020
     }
 ];
+
+//### Question 1
+
+//Create a function called `printSeason` that accepts one argument (has one parameter) called `season`.
+
+//Inside the function log the value of the argument.
+
+//Call the function and pass in the value "summer` as the argument.
+
+function printSeason (season) {
+    console.log(season);
+}
+
+printSeason("summer");
+
+//### Question 2
+
+//Create a function called `printNumbers` that has two parameters, `firstNumber` and `secondNumber`.
+
+//Inside the function log each value that is passed in.
+
+//Call the function and pass in two numbers as the arguments.
+
+function printNumbers (firstNumber,secondNumber) {
+    console.log("First number: " + firstNumber,"secound number: " + secondNumber);
+
+}
+
+printNumbers(2,4)
+
+
+//### Question 3
+
+//Create a function called `addNumbers` that has three parameters.
+
+//Inside the function add all the paramters and return the result.
+
+//Assign the result to a variable and assign the variable to the innerHTML property of the DOM element with the class of `total`.
+
+function addNumbers (num1,num2,num3){
+    const result = num1 + num2 + num3;
+    return result
+}
+
+const total = addNumbers(5,6,3);
+const totalContainer = document.querySelector(".total");
+totalContainer.innerHTML = total;
+
+//### Question 4
+
+//Create a function called `createGreeting` that has one parameter.
+
+//Inside the function add the value passed in to the string "Hello, my name is " and return the new string.
+
+//Assign the returned value to the innerHTML property of the DOM element with the id of `name`.
+
+function createGreeting(name) {
+    const Hello = "Hello, my name is " + name + ".";
+    return Hello;
+}
+
+const Hello = createGreeting("Lisa");
+const nameContainer = document.querySelector("#name");
+nameContainer.innerHTML = Hello;
+
+//### Question 5
+
+//Create a function called `printListItems` that has one parameter.
+
+//Inside the function loop through the value passed in and log each item.
+
+//Call the function and pass the `sports` array in `script.js` in as the argument.
+
+function printListItems(list){
+    for(let i = 0; i < list.length; i++)
+    console.log(list[i]);
+}
+
+printListItems(sports);
+
